74d31809d1f746a14373a1ba589883027736bcf8
Ladislav Grigorov <ladislavgrigorov@gmail.com>
2014-06-29 15:48:51 +0300

    Git free don't offer anonymous upload.

---
+++ log4netuse/log4netuse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace log4netuse
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        /// <summary>
+        /// Test method for log4net
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+
+            log.Error("Error has been reached!");
+        }
+    }
+}
---
+++ t4template/t4template/SimpleTemplate.cs
@@ -0,0 +1,16 @@
+﻿namespace t4template
+{
+	using System;
+	public class Start
+	{
+		public void Start()
+		{
+							Console.Write("5" + ", ");
+							Console.Write("4" + ", ");
+							Console.Write("3" + ", ");
+							Console.Write("2" + ", ");
+							Console.Write("1" + ", ");
+						Console.WriteLine("0" + " Start!");
+		}
+	}
+}
